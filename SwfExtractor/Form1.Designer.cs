f49bc127a6c6088d29ec4c6a92fa39bd5a84d2b3
Lee Dong-geun <incl@lunant.net>
2014-04-15 00:44:50 +0900

    Add projects

---
+++ SwfExtractor/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace SwfExtractor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.labelFileName = new System.Windows.Forms.Label();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.BtnStop = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // labelFileName
+            // 
+            this.labelFileName.AutoSize = true;
+            this.labelFileName.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.labelFileName.Location = new System.Drawing.Point(98, 21);
+            this.labelFileName.Name = "labelFileName";
+            this.labelFileName.Padding = new System.Windows.Forms.Padding(0, 2, 0, 2);
+            this.labelFileName.Size = new System.Drawing.Size(103, 16);
+            this.labelFileName.TabIndex = 1;
+            this.labelFileName.Text = "Select swf files...";
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnOpen.Location = new System.Drawing.Point(12, 12);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Padding = new System.Windows.Forms.Padding(10, 0, 5, 0);
+            this.btnOpen.Size = new System.Drawing.Size(80, 30);
+            this.btnOpen.TabIndex = 0;
+            this.btnOpen.Text = "Open";
+            this.btnOpen.UseVisualStyleBackColor = false;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 49);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(355, 23);
+            this.progressBar1.TabIndex = 2;
+            // 
+            // BtnStop
+            // 
+            this.BtnStop.Enabled = false;
+            this.BtnStop.Location = new System.Drawing.Point(374, 49);
+            this.BtnStop.Name = "BtnStop";
+            this.BtnStop.Size = new System.Drawing.Size(75, 23);
+            this.BtnStop.TabIndex = 3;
+            this.BtnStop.Text = "Stop";
+            this.BtnStop.UseVisualStyleBackColor = true;
+            this.BtnStop.Click += new System.EventHandler(this.BtnStop_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(459, 82);
+            this.Controls.Add(this.BtnStop);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.labelFileName);
+            this.Controls.Add(this.btnOpen);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Swf Extractor";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.Label labelFileName;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button BtnStop;
+    }
+}
+
---
+++ SwfExtractor/Form1.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+using ICSharpCode.SharpZipLib.Zip.Compression;
+using SwfDotNet.IO;
+using SwfDotNet.IO.ByteCode;
+using SwfDotNet.IO.Tags;
+using SwfDotNet.IO.Tags.Types;
+
+namespace SwfExtractor
+{
+    public partial class Form1 : Form
+    {
+        private BackgroundWorker _worker = null;
+        private string[] _files;
+        private Swf _swf = null;
+        private string _dir = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            _worker = new BackgroundWorker();
+            _worker.WorkerReportsProgress = true;
+            _worker.WorkerSupportsCancellation = true;
+            _worker.DoWork += new DoWorkEventHandler(Worker_DoWork);
+            _worker.ProgressChanged += new ProgressChangedEventHandler(Worker_ProgressChanged);
+            _worker.RunWorkerCompleted += new RunWorkerCompletedEventHandler(Worker_RunWorkerCompleted);
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.Filter = "SWF|*.swf";
+            dlg.Multiselect = true;
+            DialogResult res = dlg.ShowDialog();
+            if (res == DialogResult.Cancel)
+                return;
+
+            _files = dlg.FileNames;
+            _worker.RunWorkerAsync();
+            BtnStop.Text = "Stop";
+            BtnStop.Enabled = true;
+            btnOpen.Enabled = false;
+        }
+
+        private void Worker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            foreach (string fileName in _files)
+            {
+                SwfReader swfReader = new SwfReader(fileName);
+                _swf = swfReader.ReadSwf();
+                string swfDir = Path.GetDirectoryName(fileName);
+                string swfName = Path.GetFileNameWithoutExtension(fileName);
+                _dir = Path.Combine(swfDir, swfName);
+                if (!Directory.Exists(_dir))
+                    Directory.CreateDirectory(_dir);
+
+                int totalFiles = _swf.Tags.Count;
+                int counter = 0;
+                int pct = 0;
+                foreach (BaseTag tag in _swf.Tags)
+                {
+                    if (_worker.CancellationPending)
+                    {
+                        e.Cancel = true;
+                        return;
+                    }
+
+                    Export(tag, _dir);
+
+                    pct = ((++counter * 100) / totalFiles);
+                    _worker.ReportProgress(pct, fileName);
+                }
+            }
+        }
+
+        private void Worker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            labelFileName.Text = e.UserState as string;
+            progressBar1.Value = e.ProgressPercentage;
+        }
+
+        private void Worker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if (e.Error != null)
+            {
+                BtnStop.Enabled = false;
+                BtnStop.Text = "Error";
+                MessageBox.Show(e.Error.Message, "Error");
+            }
+            else if (e.Cancelled == true)
+            {
+                BtnStop.Enabled = false;
+                BtnStop.Text = "Cancelled";
+                progressBar1.Value = 0;
+            }
+            else
+            {
+                BtnStop.Enabled = true;
+                BtnStop.Text = "Done";
+            }
+
+            btnOpen.Enabled = true;
+        }
+
+        private void Export(BaseTag tag, string dir)
+        {
+            if (tag is DefineBitsJpeg2Tag)
+            {
+                DefineBitsJpeg2Tag imgTag = tag as DefineBitsJpeg2Tag;
+                imgTag.DecompileToFile(string.Format("{0}\\{1}.jpg", dir, imgTag.CharacterId));
+            }
+            else if (tag is DefineBitsJpeg3Tag)
+            {
+                DefineBitsJpeg3Tag imgTag = tag as DefineBitsJpeg3Tag;
+                imgTag.DecompileToFile(string.Format("{0}\\{1}.jpg", dir, imgTag.CharacterId));
+            }
+            else if (tag is DefineBitsLossLessTag)
+            {
+                DefineBitsLossLessTag imgTag = tag as DefineBitsLossLessTag;
+                imgTag.DecompileToFile(string.Format("{0}\\{1}.png", dir, imgTag.CharacterId));
+            }
+            else if (tag is DefineBitsLossLess2Tag)
+            {
+                DefineBitsLossLess2Tag imgTag = tag as DefineBitsLossLess2Tag;
+                imgTag.DecompileToFile(string.Format("{0}\\{1}.png", dir, imgTag.CharacterId));
+            }
+            else if (tag is DefineSoundTag)
+            {
+                DefineSoundTag soundTag = tag as DefineSoundTag;
+                if (soundTag.SoundFormat == SoundCodec.MP3)
+                    soundTag.DecompileToFile(string.Format("{0}\\{1}.mp3", dir, soundTag.CharacterId));
+                else
+                    soundTag.DecompileToFile(string.Format("{0}\\{1}.wav", dir, soundTag.CharacterId));
+            }
+        }
+
+        private void BtnStop_Click(object sender, EventArgs e)
+        {
+            if (BtnStop.Text == "Stop")
+            {
+                BtnStop.Text = "Cancelled";
+                _worker.CancelAsync();
+            }
+            else if (BtnStop.Text == "Done")
+            {
+                System.Diagnostics.Process.Start("explorer.exe", _dir);
+            }
+        }
+    }
+}
